4cb69e57f3c6b8ef52011830e341d31d124dcc91
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-02-21 00:55:38 -0600

    can win and lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     //make a variable to hold user choice and a variable to hold randomly generated choice from the other characters
 
     var enemyChoice = {};
+
     var snailUser = {};
 
     //make an object for each character with a name, an image, a value of health points, a value of attack points
@@ -15,26 +16,32 @@ $(document).ready(function () {
 
     var snail4 = {};
 
-    var snailArray = []; 
+    var snailArray = [];
+
+    var defeatedArray = [];
+
+    var snailUserIndex;
+
     makeSnails();
-
-
-
 
     //make an event listener for when user clicks on character
     $(".snailProfile").on("click", function () {
         switch (this.value) {
 
             case 'snail1': snailUser = snail1;
+                snailUserIndex = 0;
                 break;
 
             case 'snail2': snailUser = snail2;
+                snailUserIndex = 1;
                 break;
 
             case 'snail3': snailUser = snail3;
+                snailUserIndex = 2;
                 break;
 
             case 'snail4': snailUser = snail4;
+                snailUserIndex =3;
         }
         //Check if characters are choosen, if so, then display play field.
         console.log(snailUser.name)
@@ -45,20 +52,29 @@ $(document).ready(function () {
         generateEnemySnail();
         $("#attack").removeClass("hide");
         $("#battleGround").removeClass("hide");
+        $(".chooseSnail").addClass("hide");
 
     });
 
-    
+
     //trigger the computer to randomly choose a character from those remaining
     function generateEnemySnail() {
-        var randomNum = Math.floor(Math.random() * 3);
+        var randomNum = Math.floor(Math.random() * 4);
         enemyChoice = snailArray[randomNum];
-        console.log(enemyChoice);
         //make sure you have a different snail than the user
         if (enemyChoice.name === snailUser.name) {
             generateEnemySnail();
         }
         else {
+            console.log("DEFEATED ARRAY LENGTH: "+defeatedArray.length);
+            for (i = 0; i < defeatedArray.length; i++) {
+                console.log(defeatedArray[i].name);
+                if (enemyChoice.name === defeatedArray[i].name) {
+                    console.log("IN THE IF STATEMENT");
+                    generateEnemySnail();
+                    
+                }
+            }
             $("#enemyName").text(enemyChoice.name);
             $("#enemyHealth").text(enemyChoice.health);
             $("#enemyPic").attr("src", enemyChoice.enemyImage);
@@ -69,10 +85,7 @@ $(document).ready(function () {
     //Make an event listener for when the attack button is pressed
     $("#attack").on("click", function () {
         enemyChoice.health = enemyChoice.health - snailUser.attack;
-        console.log("Attack on Enemy: " + enemyChoice.health);
-        updateHealth();
         snailUser.health = snailUser.health - enemyChoice.attack;
-        console.log("Attack on User: " + snailUser.health);
         updateHealth();
         checkWin();
     });
@@ -87,40 +100,70 @@ $(document).ready(function () {
     //check to see if either character's health value = 0 or below. If so, trigger win/lose..
     function checkWin() {
         if (enemyChoice.health < 1) {
-            setTimeout(function () {
-                alert("You Win!");
-                gameOver();
-            }, 200);
+            // setTimeout(function () {
+        
+                defeatedArray.push(enemyChoice);
+
+                if (defeatedArray.length === 1){
+                    $("#defeated1").attr("src", enemyChoice.defeatImage);
+                    roundOver();
+                }
+                if (defeatedArray.length === 2){
+                    $("#defeated2").attr("src", enemyChoice.defeatImage);
+                    roundOver();
+                }
+                if (defeatedArray.length === 3){
+                    $("#defeated3").attr("src", enemyChoice.defeatImage);
+                    alert("You killed all your fellow snails! Enjoy the rest of you lonely life... :/");
+                    gameOver();
+                }
+                
+                console.log(defeatedArray);
+            // }, 200);
             // window.setTimeout(gameOver, 400);
 
         }
         else if (snailUser.health < 1) {
-            setTimeout(function () {
-                alert("You Lose!");
+                // roundOver();
+                // these reset the screen if you choose a loser snail
                 gameOver();
-            }, 200);
             // window.setTimeout(gameOver, 400);
         }
         else {
         }
     }
 
-    function gameOver() {
+    function roundOver() {
         makeSnails();
+        snailUser = snailArray[snailUserIndex];
         updateHealth();
-        console.log(snail1.health);
-        console.log(snail3.health);
+        generateEnemySnail();
+
     }
 
+    function gameOver() {
+        $("#attack").addClass("hide");
+        $(".chooseSnail").removeClass("hide");
+        $("#enemyPic").attr("src", "");
+        $("#userChoicePic").attr("src", "");
+        $("#userChoiceName").empty();
+        $("#userChoiceHealth").empty();
+        $("#enemyName").empty();
+        $("#enemyHealth").empty();
+        $("#defeated1").attr("src", "");
+        $("#defeated2").attr("src", "");
+        $("#defeated3").attr("src", "");
+        defeatedArray.length = 0;
+    }
 
     function makeSnails() {
-        snail1 = { name: "Peto", health: 100, attack: 10, image: "./assets/images/Peto-Left.png", enemyImage: "./assets/images/Peto-Right.png" };
+        snail1 = { name: "Peto", health: 100, attack: 10, image: "./assets/images/Peto-Left.png", enemyImage: "./assets/images/Peto-Right.png", defeatImage: "./assets/images/Peto-Defeated.png" };
 
-        snail2 = { name: "Greren", health: 120, attack: 6, image: "./assets/images/Grerern-Left.png", enemyImage: "./assets/images/Grerern-Right.png"};
+        snail2 = { name: "Greren", health: 120, attack: 6, image: "./assets/images/Grerern-Left.png", enemyImage: "./assets/images/Grerern-Right.png", defeatImage: "./assets/images/Greren-Defeated.png" };
 
-        snail3 = { name: "Cymbicryth", health: 110, attack: 8, image: "./assets/images/Cymb-Left.png", enemyImage: "./assets/images/Cymb-Right.png" };
+        snail3 = { name: "Cymbicryth", health: 110, attack: 8, image: "./assets/images/Cymb-Left.png", enemyImage: "./assets/images/Cymb-Right.png", defeatImage: "./assets/images/Cymb-Defeated.png" };
 
-        snail4 = { name: "Kelri", health: 105, attack: 9, image: "./assets/images/Kelri-Left.png", enemyImage: "./assets/images/Kelri-Right.png" };
+        snail4 = { name: "Kelri", health: 105, attack: 9, image: "./assets/images/Kelri-Left.png", enemyImage: "./assets/images/Kelri-Right.png", defeatImage: "./assets/images/Kelri-Defeated.png" };
 
         snailArray = [snail1, snail2, snail3, snail4];
 
